c68284d650515fdf0ecc1e628eb704b86c0a9918
triod315 <triod315@gmail.com>
2020-02-19 10:01:00 +0200

    changes

--- Lab2/Lab2.1/Lab2.1/Program.cs
+++ Lab2/Lab2.1/Lab2.1/Program.cs
@@ -1,13 +1,268 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab2._1
 {
     class Program
     {
-        
+     
         static void Main(string[] args)
         {
+            Console.Write("Enter the first number: ");
+            int mult1 = int.Parse((Console.ReadLine()));
+            Console.Write("Enter the second number: ");
+            int mult2= int.Parse((Console.ReadLine()));
+            Console.WriteLine();
             
+            BoothAlgorithm(mult1,mult2);
+
+        }
+        
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        static int BoothAlgorithm(int numb1, int numb2)
+        {
+            int m = numb1, r = numb2;         
+
+            // numb1
+            List<int> a = new List<int>();
+            // -numb1
+            List<int> s = new List<int>();
+            // numb2
+            List<int> p = new List<int>();
+
+            Get2PowKBitsNumb(m, r, out a, out p, out s);
+
+            int x = a.Count;
+            int y = p.Count;
+
+            ComplementForList(a, y + 1);
+            ComplementForList(s, y + 1);
+            ComplementForListForP(p, x);
+
+            Console.WriteLine($"a: \t{BitsToString(a)}");
+            Console.WriteLine($"s: \t{BitsToString(s)}");
+            Console.WriteLine($"p: \t{BitsToString(p)}");
+
+            for (int i = 0; i < y; i++)
+            {
+                if (p[p.Count - 1] == 1 && p[p.Count - 2] == 0)
+                {
+                    AddBinary(p, a);
+                    Console.WriteLine($"SUB\np + a \t\t{BitsToString(p)}");
+                }
+                else if (p[p.Count - 1] == 0 && p[p.Count - 2] == 1)
+                {
+                    AddBinary(p, s);
+                    Console.WriteLine($"ADD\np + s\t\t{BitsToString(p)}");
+                }
+                else
+                {
+                    Console.WriteLine("NOP");
+                }
+                RightShift(p);
+                Console.WriteLine($"right shift\t{BitsToString(p)}");
+            }
+            p.RemoveAt(p.Count - 1);
+            Console.WriteLine($"result \t\t{BitsToString(p)}");
+            int result = GetNumbFromTwoComplement(p);
+            Console.WriteLine($"\nResult in base 10: {result}");
+            return result;
+        }
+
+        private static void ComplementForListForP(List<int> p, int x)
+        {
+            List<int> helper = new List<int>() { 0 };
+            while(x > 0)
+            {
+                helper.AddRange(p);
+                p.Clear();
+                p.AddRange(helper);
+                helper.RemoveRange(1, helper.Count - 1);
+                x--;
+            }
+            p.Add(0);
+        }
+
+        static int GetNumbFromTwoComplement(List<int> p)
+        {
+            int result = -p[0] * (int)Math.Pow(2, p.Count - 1);
+            for (int i = p.Count - 1; i > 0; i--)
+            {
+                result += p[i] * (int)Math.Pow(2, p.Count - i - 1);
+            }
+            return result;
+        }
+        static string BitsToString(List<int> list)
+        {
+            string result = "";
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i % 8 == 0 && i!=0)
+                    result += " ";
+                result += list[i];
+            }
+            return result;
+        }
+        static void AddBinary(List<int> p, List<int> aOrs)
+        {
+            int mind = 0;
+            for (int i = p.Count - 1; i >= 0; i--)
+            {
+                if ((p[i] == 0 && aOrs[i] == 1 && mind == 0) || (p[i] == 1 && aOrs[i] == 0 && mind == 0) || (p[i] == 0 && aOrs[i] == 0 && mind == 1))
+                {
+                    p[i] = 1;
+                    mind = 0;
+                }
+                else if ((p[i] == 0 && aOrs[i] == 1 && mind == 1) || (p[i] == 1 && aOrs[i] == 0 && mind == 1) || (p[i] == 1 && aOrs[i] == 1 && mind == 0))
+                {
+                    p[i] = 0;
+                    mind = 1;
+                }
+                else if (p[i] == 0 && aOrs[i] == 0 && mind == 0)
+                {
+                    p[i] = 0;
+                    mind = 0;
+                }
+                else
+                {
+                    p[i] = 1;
+                    mind = 1;
+                }
+            }
+        }
+        static void RightShift(List<int> p)
+        {
+            for (int i = p.Count - 1; i > 0; i--)
+            {
+                p[i] = p[i - 1];
+            }
+        }
+        static void ComplementForList(List<int> bits, int count)
+        {
+            while(count > 0)
+            {
+                bits.Add(0);
+                count--;
+            }
+        }
+        static List<int> GetBitsFromNumb(int numb)
+        {
+            List<int> bits = new List<int>();
+            if (numb > 0)
+            {
+                while (numb != 0)
+                {
+                    bits.Add(numb % 2);
+                    numb = numb / 2;
+                }
+                bits.Reverse();
+            }
+            else
+            {
+                bits = GetBitsFromNumb(-numb);
+                Inversion(bits);
+                AddOne(bits);
+                List<int> addOne = new List<int>() { 1 };
+                addOne.AddRange(bits);
+                return addOne;
+            }
+            return bits;
+        }
+        static void Get2PowKBitsNumb(int numb1, int numb2, out List<int> first, out List<int> second, out List<int> invertFirst)
+        {
+            first = GetBitsFromNumb(numb1);
+            second = GetBitsFromNumb(numb2);
+            invertFirst = GetBitsFromNumb(-numb1);
+
+            int maxCount = second.Count;
+            if (invertFirst.Count > first.Count && invertFirst.Count > second.Count)
+            {
+                maxCount = invertFirst.Count;
+            }
+            else if (first.Count > second.Count)
+            {
+                maxCount = first.Count;
+            }
+            int lenght = (int)Math.Log(maxCount, 2) + 1;
+            lenght = (int)Math.Pow(2, lenght);
+
+            int elem1 = (numb1 < 0) ? 1 : 0;
+            List<int> helper = new List<int>() { elem1 };
+            int countOfElem = first.Count;
+            for (int i = 0; i < lenght - countOfElem; i++)
+            {
+                helper.AddRange(first);
+                first.Clear();
+                first.AddRange(helper);
+                helper.RemoveRange(1, helper.Count - 1);
+            }
+            int elem2 = (numb2 < 0) ? 1 : 0;
+            helper = new List<int>() { elem2 };
+            countOfElem = second.Count;
+            for(int i = 0; i < lenght - countOfElem; i++)
+            {
+                helper.AddRange(second);
+                second.Clear();
+                second.AddRange(helper);
+                helper.RemoveRange(1, helper.Count - 1);
+            }
+            int elem3 = ((-numb1) < 0) ? 1 : 0;
+            countOfElem = invertFirst.Count;
+            helper = new List<int>() { elem3 };
+            for (int i = 0; i < lenght - countOfElem; i++)
+            {
+                helper.AddRange(invertFirst);
+                invertFirst.Clear();
+                invertFirst.AddRange(helper);
+                helper.RemoveRange(1, helper.Count - 1);
+            }
+        }
+        static void Inversion(List<int> bits)
+        {
+            for (int i = 0; i < bits.Count; i++)
+            {
+                if (bits[i] == 0)
+                {
+                    bits[i] = 1;
+                }
+                else
+                {
+                    bits[i] = 0;
+                }
+            }
+        }
+        static List<int> AddOne(List<int> bits)
+        {
+            int mind = 1;
+            for (int i = bits.Count - 1; i >= 0; i--)
+            {
+                if (i == 0 && bits[i] == 1 && mind == 1)
+                {
+                    List<int> newBits = new List<int>();
+                    newBits.AddRange(new List<int>() { 1, 1 });
+                    newBits.AddRange(bits);
+                    return newBits;
+                }
+                else if (bits[i] == 0 && mind == 1)
+                {
+                    bits[i] = 1;
+                    mind = 0;
+                }
+                else if (bits[i] == 1 && mind == 1)
+                {
+                    bits[i] = 0;
+                }
+            }
+            return bits;
         }
     }
 }
---
+++ Lab2/Lab2.3/Lab2.3/Program.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2._3
+{
+    class Program
+    {
+        static bool flag = false;
+        static int counterOverFlow = 0;
+        static void Main(string[] args)
+        {
+            float numb1, numb2;
+
+            numb1 = float.Parse(Console.ReadLine());
+            numb2 = float.Parse(Console.ReadLine());
+            
+            FloatPointNumb first = new FloatPointNumb();
+            FloatPointNumb second = new FloatPointNumb();
+
+            FloatPointNumb result = new FloatPointNumb();
+
+            string numb1InBin = FloatToBinary(numb1);
+            string numb2InBin = FloatToBinary(numb2);
+
+            first.Sign = Convert.ToInt32(numb1InBin.Split(' ')[0]);
+            first.Mantissa = numb1InBin.Split(' ')[2].Select(c => Int32.Parse(c.ToString())).ToList();
+            first.Exponent = numb1InBin.Split(' ')[1].Select(c => Int32.Parse(c.ToString())).ToList();
+            Console.WriteLine($"{numb1}\t{first.Sign} {GetListAsStr(first.Exponent)} {GetListAsStr(first.Mantissa)} ");
+            first.Exponent = Subtract(first.Exponent, new List<int>() { 0, 1, 1, 1, 1, 1, 1, 1 });
+
+            second.Sign = Convert.ToInt32(numb2InBin.Split(' ')[0]);
+            second.Mantissa = numb2InBin.Split(' ')[2].Select(c => Int32.Parse(c.ToString())).ToList();
+            second.Exponent = numb2InBin.Split(' ')[1].Select(c => Int32.Parse(c.ToString())).ToList();
+            Console.WriteLine($"{numb2}\t{second.Sign} {GetListAsStr(second.Exponent)} {GetListAsStr(second.Mantissa)}");
+            second.Exponent = Subtract(second.Exponent, new List<int>() { 0, 1, 1, 1, 1, 1, 1, 1 });
+
+            result.Sign = ((first.Sign == 0 && second.Sign == 0) || (first.Sign == 1 && second.Sign == 1)) ? 0 : 1;
+            result.Exponent = ADD(first.Exponent, second.Exponent, 0);
+            Console.WriteLine($"E1+E2\t {GetListAsStr(result.Exponent)}");
+            
+            result.Mantissa = multiplicate(first.Mantissa, second.Mantissa);
+            if (counterOverFlow >= 1 && (first.Sign == 0 && second.Sign == 0))
+               result.Exponent = ADD(result.Exponent, new List<int>() { 0, 0, 0, 0, 0, 0, 0, 1}, 0);
+             result.Exponent = ADD(result.Exponent, new List<int>() { 0, 1, 1, 1, 1, 1, 1, 1 }, 0);
+            string resultStr = result.Sign + GetListAsStr(result.Exponent) + GetListAsStr(result.Mantissa);
+            
+            Console.WriteLine($"result: {result.Sign} {GetListAsStr(result.Exponent)} {GetListAsStr(result.Mantissa)}");
+
+            float res = BinaryStringToSingle(resultStr);
+            Console.WriteLine(res);
+            
+        }
+        static List<int> Subtract(List<int> exponent, List<int> numb)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = exponent.Count - 1; i >= 0; i--)
+            {
+                if (exponent[i] == 0 && numb[i] == 0)
+                {
+                    result.Add(0);
+                }
+                else if (exponent[i] == 1 && numb[i] == 1)
+                {
+                    result.Add(0);
+                }
+                else if (exponent[i] == 1 && numb[i] == 0)
+                {
+                    result.Add(1);
+                }
+                else
+                {
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (exponent[j] == 1)
+                        {
+                            for (int k = j + 1; k <= i; k++)
+                            {
+                                if (exponent[k] == 0)
+                                    exponent[k] = 1;
+                            }
+                            exponent[j] = 0;
+                            result.Add(1);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            result.Reverse();
+            return result;
+        }
+        static string GetListAsStr(List<int> list)
+        {
+            string result = string.Join("", list.ToArray());
+            return result;
+        }
+        static float BinaryStringToSingle(string s)
+        {
+            long i = Convert.ToInt64(s, 2);
+            byte[] b = BitConverter.GetBytes(i);
+            return BitConverter.ToSingle(b, 0);
+        }
+        static List<int> multiplicate(List<int> first, List<int> second)
+        {
+            List<int> result = new List<int>();
+            bool flag = false;
+
+            List<int> help = new List<int>() { 1 };
+            help.AddRange(first);
+            first.Clear();
+            first.AddRange(help);
+            help.Clear();
+            help.Add(1);
+            help.AddRange(second);
+            second.Clear();
+            second.AddRange(help);
+
+            List<int> helper1 = new List<int>();
+            List<int> helper2 = new List<int>();
+
+            if (second[second.Count - 1] == 0)
+            {
+                helper1.AddRange(new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            }
+            else
+            {
+                helper1.AddRange(first);
+            }
+
+            List<int> helper = new List<int>();
+            int shift = 1;
+            for(int i = second.Count - 2; i >= 0; i--)
+            {
+                helper2.Clear();
+                if (second[i] == 0)
+                {
+                    helper2.AddRange(new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+                }
+                else
+                {
+                    helper2.AddRange(first);
+                }
+                helper.AddRange(helper1);
+                helper1.Clear();
+                shift = helper.Count - helper2.Count + 1;
+                helper1.AddRange(ADD(helper, helper2, shift));
+                //Console.WriteLine($"First addition: {GetListAsStr(helper)}   Second addition: {GetListAsStr(helper2)}   Sum: {GetListAsStr(helper1)}");
+                helper.Clear();
+            }
+
+            for (int i = 0; i < helper1.Count; i++)
+            {
+                while(helper1[0] == 0)
+                {
+                    helper1.RemoveAt(0);
+                }
+                helper1.RemoveAt(0);
+                break;
+            }
+            List<int> helperForHelp = new List<int>();
+            helperForHelp.AddRange(helper1);
+            helper1.Clear();
+            for(int i = 0; i < 23; i++)
+            {
+                helper1.Add(helperForHelp[i]);
+            }
+            Console.WriteLine($"M1*M2 \t{GetListAsStr(helper1)}");
+
+            return helper1;
+        }
+        static List<int> ADD(List<int> first, List<int> second, int shift)
+        {
+            List<int> result = new List<int>();
+
+            if (shift > 0 && !Program.flag)
+            {
+                List<int> help = new List<int>() { 0 };
+                help.AddRange(first);
+                first.Clear();
+                first.AddRange(help);
+            }
+            Program.flag = false;
+
+            while(shift > 0)
+            {
+                second.Add(0);
+                shift--;
+            }
+
+            int mind = 0;
+            for (int i = first.Count - 1; i >= 0; i--)
+            {
+                if (first[i] == 0 && second[i] == 0 && mind == 0)
+                {
+                    result.Add(0);
+                }
+                else if ((first[i] == 0 && second[i] == 0 && mind == 1) ||
+                        (first[i] == 0 && second[i] == 1 && mind == 0) ||
+                        (first[i] == 1 && second[i] == 0 && mind == 0))
+                {
+                    result.Add(1);
+                    mind = 0;
+                }
+                else if ((first[i] == 0 && second[i] == 1 && mind == 1) ||
+                        (first[i] == 1 && second[i] == 0 && mind == 1) ||
+                        (first[i] == 1 && second[i] == 1 && mind == 0))
+                {
+                    result.Add(0);
+                    mind = 1;
+                }
+                else
+                {
+                    result.Add(1);
+                    mind = 1;
+                }
+            }
+            if (mind == 1)
+            {
+                result.Add(1);
+                Program.flag = true;
+                Program.counterOverFlow++;
+            }
+
+            result.Reverse();
+            return result;
+        }
+        static string FloatToBinary(float f)
+        {
+            StringBuilder sb = new StringBuilder();
+            Byte[] ba = BitConverter.GetBytes(f);
+            foreach (Byte b in ba)
+                for (int i = 0; i < 8; i++)
+                {
+                    sb.Insert(0, ((b >> i) & 1) == 1 ? "1" : "0");
+                }
+            string s = sb.ToString();
+            string r = s.Substring(0, 1) + " " + s.Substring(1, 8) + " " + s.Substring(9);
+            return r;
+        }
+    }
+    class FloatPointNumb
+    {
+        public List<int> Exponent { get; set; }
+        public List<int> Mantissa { get; set; }
+        public int Sign { get; set; }
+    }
+}
