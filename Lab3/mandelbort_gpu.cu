f45f1d3ea0b330cdb5de171a1cc4ab96360b0276
triod315 <triod315@gmail.com>
2020-02-27 11:37:11 +0200

    Lab3

---
+++ Lab3/mandelbort_gpu.cu
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <complex>
+
+using namespace std;
+
+float width = 4800;
+float height = 4800; 
+
+
+__device__ int value (int x, int y)  {
+    complex<float> point((float)x/width-1.5, (float)y/height-0.5);
+    complex<float> z(0, 0);
+    int nb_iter = 0;
+    while (abs (z) < 2 && nb_iter <= 20) {
+        z = z * z + point;
+        nb_iter++;
+    }
+    if (nb_iter < 20)
+       return (255*nb_iter)/20;
+    else 
+       return 0;
+}
+
+int main (int argc, char* argv[])  {
+
+    string filename=argv[0]+string(".ppm");
+    ofstream my_Image (filename);
+
+    width=atoi(argv[1]);
+    height=atoi(argv[2]);
+
+    if (my_Image.is_open ()) {
+        my_Image << "P3\n" << width << " " << height << " 255\n";
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++)  {
+                int val = value(i, j);
+                my_Image << 0 << ' ' << val << ' ' << val/4 << "\n";
+            }
+        }
+        my_Image.close();
+    }
+    else
+      cout << "Could not open the file";
+    
+    return 0;
+}
---
+++ Lab3/script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+compiler_flags=(O0 O1 O2 O3 Os Ofast)
+
+flagsout="{"
+time_list="{"
+
+for flag in ${compiler_flags[@]}
+do
+    filename=gpp$1$flag
+    echo -$flag $1 -o $filename
+    g++ -$flag $1 -o $filename
+    T=`(time -p ./$filename $2 $3) 2>&1 | grep real | awk '{print ($2)'} | sed 's/,/./'`
+    flagsout=$flagsout", "\"$flag\"
+    time_list=$time_list", "$T
+done
+
+echo $flagsout}>gnuresult.txt
+echo $time_list}>>gnuresult.txt
+flagsout="{"
+time_list="{"
+
+icc_cpu_flags=(sse2 ssse3 sse4.1 sse4.2)
+
+for iccflag in ${icc_cpu_flags[@]}
+do
+    filename=icc$1$iccflag
+    echo -x$iccflag $1 -o $filename
+    icc -x$iccflag -Ofast $1 -o $filename
+    T=`(time -p ./$filename $2 $3) 2>&1 | grep real | awk '{print ($2)'} | sed 's/,/./'`
+    flagsout=$flagsout", "\"$iccflag\"
+    time_list=$time_list", "$T
+done
+
+echo $flagsout}>intelresult.txt
+echo $time_list}>>intelresult.txt
+
+rm gp*
+rm ic*
