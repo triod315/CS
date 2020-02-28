182f01d4fbf964d2eb0d34ce70e9a78c64d0665e
triod315 <triod315@gmail.com>
2020-02-28 12:56:59 +0200

    changes in script

--- Lab3/script.sh
+++ Lab3/script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 uind=`date | awk '{print $4}' | awk -F: '{print $1$2$3}'`
-echo $uind
-#file=cat $1
-#echo $file>code$uind 
+echo $uind>log$uind
 compiler_flags=(O0 O1 O2 O3 Os Ofast)
 
 flagsout="{"
@@ -11,12 +9,14 @@ time_list="{"
 
 for flag in ${compiler_flags[@]}
 do
-    filename=gpp$1$flag
-    echo -$flag $1 -o $filename
+    filename="$1$flag$uind"gpp
+    echo -$flag $1 -o $filename>>log$uind
     g++ -$flag $1 -o $filename -std=c++11
-    T=`(time -p ./$filename $2 $3) 2>&1 | grep real | awk '{print ($2)'} | sed 's/,/./'`
+    T=`(time -p $filename $2 $3) 2>&1 | grep real | awk '{print ($2)'} | sed 's/,/./'`
     flagsout=$flagsout", "\"$flag\"
     time_list=$time_list", "$T
+    rm $filename
+    rm "$filename".ppm
 done
 
 echo $2 x $3>"gnuresult$uind"
@@ -29,17 +29,16 @@ icc_cpu_flags=(sse2 ssse3 sse4.1 sse4.2)
 ml icc
 for iccflag in ${icc_cpu_flags[@]}
 do
-    filename=icc$1$iccflag
-    echo -x$iccflag $1 -o $filename
+    filename="$1$iccflag$unid"icc
+    echo -x$iccflag $1 -o $filename>>log$uind
     icc -x$iccflag -Ofast $1 -o $filename -std=c++11
-    T=`(time -p ./$filename $2 $3) 2>&1 | grep real | awk '{print ($2)'} | sed 's/,/./'`
+    T=`(time -p $filename $2 $3) 2>&1 | grep real | awk '{print ($2)'} | sed 's/,/./'`
     flagsout=$flagsout", "\"$iccflag\"
     time_list=$time_list", "$T
+    rm "$filename"
+    rm "$filename".ppm
 done
 
 echo $2 x $3>intelresult$uind
 echo $flagsout}>>intelresult$uind
 echo $time_list}>>intelresult$uind
-
-rm gp*
-rm ic*
